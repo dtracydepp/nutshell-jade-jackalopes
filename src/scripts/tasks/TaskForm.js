5dfbffdd46cfec2ddfa3a9b3e15f56c2bad48d8a
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-17 11:36:10 -0600

    added TaskForm function, render function, and event listener for saveTask click event

---
+++ src/scripts/tasks/TaskForm.js
@@ -0,0 +1,35 @@
+import { getTasks, saveTask, useTasks } from "./TaskProvider"
+
+
+const contentTarget = document.querySelector(".taskModalContainer")
+const eventHub = document.querySelector(".container")
+
+export const TaskForm = () => {
+    getTasks().then(() => {
+        const listOfTasks = useTasks()
+        // console.log(listOfTasks)
+        render(listOfTasks)
+    })
+}
+
+const render = () => {
+    contentTarget.innerHTML = `
+    <textarea id="task--name" placeholder="What Is Your Task?"></textarea>
+    <p>Task To Be Completed By:</p>
+    <input id="task--date" type="date"/>
+    <button id="saveTask">Submit</button>
+    `
+}
+
+eventHub.addEventListener("click", event => {
+    if (event.target.id === "saveTask") {
+        const name = document.querySelector("#task--name").value
+        const date = document.querySelector("#task--name").value
+        
+        const newTask = {
+            name,
+            date
+        }
+        saveTask(newTask)
+    }
+})

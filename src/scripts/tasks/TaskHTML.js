b25158ebd85395196a38ee717a77940cfb8746dc
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-17 13:18:46 -0600

    added delete button html

--- src/scripts/tasks/TaskHTML.js
+++ src/scripts/tasks/TaskHTML.js
@@ -4,6 +4,7 @@ export const Task = (taskObj) => {
     <h3 class="task--name">Task: ${taskObj.name}</h3>
     <p class="task--completeBy">To Be Completed By: ${taskObj.date}</p>
     <input class="task--completeStatus" type="checkbox"/>
+    <button id="deleteTask--${taskObj.id}">Delete Task</button>
     </section>
     `
 }

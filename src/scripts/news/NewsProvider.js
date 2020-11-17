f5e0409e46cde0b60fe822c3a163b8085078e9e5
dtracydepp <tracydepp82@gmail.com>
2020-11-17 11:22:59 -0600

    added news to json and created news modules and newprovider module

---
+++ src/scripts/news/NewsProvider.js
@@ -0,0 +1,46 @@
+// Keeps track of the news data
+
+const eventHub = document.querySelector(".container")
+
+const dispatchStateChangeEvent = () => {
+    const newsStateChangedEvent = new CustomEvent("newsStateChanged")
+
+    eventHub.dispatchEvent(newsStateChangedEvent)
+}
+
+let news = []
+
+export const getArticles = () => {
+    return fetch('http://localhost:8088/news')
+        .then(response => response.json())
+        // not sure if I need this
+            .then(parsedNews => {
+            news = parsedNews
+        })
+
+}
+// not sure I need this
+export const useArticles = () => {
+    return news.slice()
+}
+
+export const saveArticles =(news) => {
+return fetch('http://localhost:8088/news', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(news)
+    })
+    .then(getArticles)
+
+    .then(dispatchStateChangeEvent) 
+}
+
+export const deleteArticle = userId => {
+    return fetch(`http://localhost:8088/news/${userId}`, {
+        method: "DELETE"
+    })
+        .then(getArticles)
+
+}

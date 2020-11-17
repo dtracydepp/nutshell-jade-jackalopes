91dc69fe2b3e02c50b461a2753e1c1d02bd09723
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-17 11:16:29 -0600

    added getTask, useTask, saveTask, and deleteTask functions

---
+++ src/scripts/tasks/TaskProvider.js
@@ -0,0 +1,40 @@
+const eventHub = document.querySelector(".container")
+const dispatchStateChangeEvent = () => {
+    const taskStateChangedEvent = new CustomEvent("taskStateChanged")
+
+    eventHub.dispatchEvent(taskStateChangedEvent)
+}
+
+let tasks = []
+
+export const getTasks = () => {
+    return fetch ("http://localhost:8088/tasks")
+    .then(response => response.json())
+    .then(parsedTasks => {
+        tasks = parsedTasks
+        console.log(parsedTasks)
+    })
+}
+
+export const useTasks = () => {
+    return tasks.slice()
+}
+
+export const saveTask = task => {
+    return fetch("http://localhost:8088/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(task)
+    })
+    .then(getTasks)
+    .then(dispatchStateChangeEvent)
+}
+
+export const deleteTask = taskId => {
+    return fetch (`http://localhost:8088/tasks"/${taskId}`, {
+        method: "DELETE"
+    })
+        .then(getTasks)
+}

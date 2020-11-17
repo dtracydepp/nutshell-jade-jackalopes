4936ab8ee4d8a161a60917b0dc3aefb45f0c86ff
Alan Andrusky <alanandrusky@gmail.com>
2020-11-17 16:23:38 -0600

    new push to get rid of commented out trouble-shooting api key

--- src/scripts/Weather/WeatherProvider.js
+++ src/scripts/Weather/WeatherProvider.js
@@ -7,7 +7,7 @@ export const getWeatherItems = (eventLocation) => {
     console.log("getting adsfgsdf", apiObject.weatherKey)
     
   return fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${eventLocation},us&appid=${apiObject.weatherKey}&units=imperial`)
-  //return fetch(`  https://api.openweathermap.org/data/2.5/weather?zip=16441,us&appid=bc4f6eef7b237249d1ac8322c96f63d5&units=imperial`)
+  
       .then(
           response => response.json())
       .then(

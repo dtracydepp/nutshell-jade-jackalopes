f4a85122d16d570e098268a3fc47d1618ba3c2fa
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-18 16:07:41 -0600

    created getChat, useChat, sendChat, and deleteMessage functions

---
+++ src/scripts/chat/ChatProvider.js
@@ -0,0 +1,40 @@
+const eventHub = document.querySelector(".container")
+const dispatchStateChangeEvent = () => {
+    const chatStateChangedEvent = new CustomEvent("chatStateChanged")
+
+    eventHub.dispatchEvent(chatStateChangedEvent)
+}
+
+let chats = []
+
+export const getChats = () => {
+    return fetch ("http://localhost:8088/chats")
+    .then(response => response.json())
+    .then(parsedChats => {
+        chats = parsedChats
+        // console.log("parsedChats:", parsedChats)
+    })
+}
+
+export const useChats = () => {
+    return chats.slice()
+}
+
+export const sendChat = chat => {
+    return fetch("http://localhost:8088/chats", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(chat)
+    })
+    .then(getChats)
+    .then(dispatchStateChangeEvent)
+}
+
+export const deleteMessage = chatId => {
+    return fetch (`http://localhost:8088/chats/${chatId}`, {
+        method: "DELETE"
+    })
+        .then(getChats)
+}

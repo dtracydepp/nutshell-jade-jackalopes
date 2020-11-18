dc082c54637a813c1e6da55fbe38021adae6c588
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-18 12:20:24 -0600

    refactored TaskList function to include a getItem for the current active user, and a filter for tasks that are not complete and with a userId of the current active user

--- src/scripts/tasks/TaskList.js
+++ src/scripts/tasks/TaskList.js
@@ -9,13 +9,14 @@ eventHub.addEventListener("taskStateChanged", () => TaskList())
 export const TaskList = () => {
     getTasks()
     .then(() => {
-        const tasks = useTasks()
+        const activeUser = parseInt(sessionStorage.getItem("activeUser")) //searches session storage to find the current active user
+        const tasks = useTasks().filter(task => task.isComplete === false && task.userId === activeUser) // filters the useTasks array for isComplete = false (tasks that have not been completed) and for tasks with a userId value that matches the current active user
         render(tasks)
     })
 }
 
 const render = (taskCollection) => {
-    let taskHTMLRepresentation = ""
+    let taskHTMLRepresentation = "" 
     for (const task of taskCollection) {
         taskHTMLRepresentation += Task(task)
     }
@@ -25,10 +26,9 @@ const render = (taskCollection) => {
     `
 }
 
-eventHub.addEventListener("click", deleteEvent => {
-    if (deleteEvent.target.id.startsWith("delete")) {
-        const [prefix, id] = deleteEvent.target.id.split("--")
-
+eventHub.addEventListener("click", event => {
+    if (event.target.id.startsWith("delete")) {
+        const [prefix, id] = event.target.id.split("--")
         deleteTask(id).then(
             () => {
                 const deleteTask = useTasks()
@@ -37,16 +37,3 @@ eventHub.addEventListener("click", deleteEvent => {
         )   
     }
 })
-
-// eventHub.addEventListener("click", hideEvent => {
-//     if (hideEvent.target.id.startsWith("hide")) {
-//         const [prefix, id] = hideEvent.target.id.split("--")
-
-//         hideTask(id).then(
-//             () => {
-//                 const hideTask = useTasks()
-//                 render(hideTask)
-//             }
-//         )
-//     }
-// })

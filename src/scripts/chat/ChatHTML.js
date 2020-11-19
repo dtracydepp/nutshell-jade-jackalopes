6ec075073a8cbcdfc17fad8706d2b8335ac14bd7
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-19 13:10:00 -0600

    added userObj parameter

--- src/scripts/chat/ChatHTML.js
+++ src/scripts/chat/ChatHTML.js
@@ -2,7 +2,7 @@ export const Chat = (chatObj) => {
     console.log("chat object:", chatObj)
     return `
     <section class="chat">
-    <h5 class="chat--name">${chatObj.userId}</h5>
+    <h5 id="${chatObj.user.id}" class="chat--name">User: ${chatObj.user.username}</h5>
     <p class="chat--message">Message: ${chatObj.message}</p>
     <button id="deleteChat--${chatObj.id}">Delete Message</button>
     </section>

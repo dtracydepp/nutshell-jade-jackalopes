7bed76af55130c964d03ea0e9f68790f28dcb2c3
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-19 13:11:15 -0600

    started writing getUserChats and getUsername functions, wip

--- src/scripts/chat/ChatList.js
+++ src/scripts/chat/ChatList.js
@@ -6,28 +6,31 @@ const chatContainer = document.querySelector(".chatContainer");
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("chatStateChanged", () => ChatList())
+let chats = []
 
 export const ChatList = () => {
-    getChats()
+    getChats() //getUsers, .map & find
     .then(() => {
-        const chats = useChats()
-        render(chats)
+        chats = useChats()
     })
+    .then(render)
 }
 
-const render = (chatCollection) => {
+const render = () => {
+    if (sessionStorage.activeUser) {
     let chatHTMLRepresentation = ""
-    for (const chat of chatCollection) {
-        chatHTMLRepresentation += Chat(chat)
+    for (const chat of chats) {
+        chatHTMLRepresentation += Chat(chat) // put this in forEach, don't need for of from line 22
     }
     chatContainer.innerHTML = `
     <h3>Messages:</h3>
     ${chatHTMLRepresentation}
     `
 }
+}
 
 eventHub.addEventListener("click", event => {
-    if (event.target.id.startsWith("delete")) {
+    if (event.target.id.startsWith("deleteChat")) {
         const [prefix, id] = event.target.id.split("--")
         deleteMessage(id).then(
             () => {
@@ -37,3 +40,18 @@ eventHub.addEventListener("click", event => {
         )   
     }
 })
+
+let users = []
+let userChats = []
+
+const getUserChats = (user) => {
+    const allChats = userChats.filter(chatMessages => chatMessages.userId === user.id) //no need to filter, need to use forEach 
+    return allChats
+}
+
+export const getUsername =  username => {
+    const userObj = username.map(userChat => {
+        return users.find(user => user.id === userChat.userId)
+    })
+    return userObj
+}

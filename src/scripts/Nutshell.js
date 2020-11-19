defc85c321456bc375aeab46e0157894e7e49d1e
ericaphillips <ericaphill@gmail.com>
2020-11-19 15:59:47 -0600

    fixed nuttyshell

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,6 +1,7 @@
 import { TaskForm } from "./tasks/TaskForm.js"
 import { TaskList } from "./tasks/TaskList.js"
-import "./news/NewsForm.js"     
+import { NewsList } from "./news/NewsList.js"     
+import { makeNoteForm } from "./news/NewsForm.js"
 
 import "./events/EventProvider.js"
 import { EventForm } from "./events/EventForm.js"
@@ -20,15 +21,16 @@ TaskList()
 EventForm()
 EventList()
 NewsList()
+makeNoteForm()
 ChatForm()
 ChatList()
 
-const eventHub = document.querySelector(".container")
-eventHub.addEventListener("click", clickEvent => {
-    if(clickEvent.target.id ==="newarticle"){
-    // console.log("I hear you")
-    const newArticle = new CustomEvent("addNewArticle")
-    eventHub.dispatchEvent(newArticle)
-    }
-})
+ const eventHub = document.querySelector(".container")
+ eventHub.addEventListener("click", clickEvent => {
+  if(clickEvent.target.id ==="newarticle"){
+     // console.log("I hear you")
+   const newArticle = new CustomEvent("addNewArticle")
+     eventHub.dispatchEvent(newArticle)
+     }
+ })
 }

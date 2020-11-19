8bf71c60df806bacb7e632d208747daf58e54e15
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-19 14:52:25 -0600

    added id of userObj.id

--- src/scripts/chat/ChatHTML.js
+++ src/scripts/chat/ChatHTML.js
@@ -1,10 +1,12 @@
-export const Chat = (chatObj, UserObj) => {
+export const Chat = (chatObj, userObj) => {
     console.log("chat object:", chatObj)
     return `
     <section class="chat">
-    <h5 id="${chatObj.user.id}" class="chat--name">User: ${chatObj.user.username}</h5>
+    <h5 id="${userObj.id}" class="chat--name">User: ${userObj.username}</h5>
     <p class="chat--message">Message: ${chatObj.message}</p>
     <button id="deleteChat--${chatObj.id}">Delete Message</button>
     </section>
     `
 }
+
+//make line 5 an anchor link

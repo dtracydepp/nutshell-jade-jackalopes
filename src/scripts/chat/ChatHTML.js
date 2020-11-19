9ce6083b774e09cc42ded0ac13617533a61d827b
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-19 15:50:53 -0600

    added anchor link text to chat username

--- src/scripts/chat/ChatHTML.js
+++ src/scripts/chat/ChatHTML.js
@@ -5,11 +5,10 @@ export const Chat = (chatObj, userObj) => {
     console.log("chat object:", chatObj)
     return `
     <section class="chat">
-    <h5 id="${userObj.id}" class="chat--name">User: ${userObj.username}</h5>
+    <h5 id="${userObj.id}" class="chat--name"><a href="">User: ${userObj.username}</a></h5>
     <p class="chat--message">Message: ${chatObj.message}</p>
     <button id="deleteChat--${chatObj.id}">Delete Message</button>
     </section>
     `
 }
 
-//make line 5 an anchor link

0e495181f973465838f3314d8675c7c4bf5a1226
ericaphillips <ericaphill@gmail.com>
2020-11-20 08:48:49 -0600

    fixed some news stuff

--- src/scripts/news/NewsHTML.js
+++ src/scripts/news/NewsHTML.js
@@ -6,7 +6,7 @@ export const NewsAsHTML =(newsObject) => {
     <div class="newsContainer">
         <div class "news--title">Title: ${newsObject.title}</div>
         <div class "news--synopsis">Synopsis: ${newsObject.synopsis}</div>
-        <div class "news--url">Link to Article: ${newsObject.url}</div>
+        <div class "news--url">Link: <a href="${newsObject.url}">${newsObject.url}</a></div>
         <p>${new Date(newsObject.timestamp).toLocaleDateString('en-US')}</p>
        <button id="deleteNote--${newsObject.id}">Delete</button>
     </div>
